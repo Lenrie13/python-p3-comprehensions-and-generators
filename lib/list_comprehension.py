e534e2f8b91b7e6b839a5193063d33e410859ad5
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-09-12 15:15:26 +0300

    update files

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+# Function to return a list of even numbers from num_list
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
+# Function to add exclamation marks to each sentence in sentence_list
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
